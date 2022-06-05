031a5e679fb43ba526b99d23d49ebd146e762db8
Ki <kimkajog@gmail.com>
2022-06-05 11:51:19 +0900

    signup 비지니스 로직

--- model/user.js
+++ model/user.js
@@ -1,7 +1,33 @@
 import mongoose from "mongoose";
 
 const userSchema = mongoose.Schema(
-    {},
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        phone: {
+            type: Number,
+        },
+        age: {
+            type: Number,
+        },
+        birth: {
+            type: Date,
+        },
+        profileImg: {
+            type: String,
+        }
+
+    },
     {
         timestamps: true,
     }
--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,54 @@
 import express from "express";
 import asyncHandler from "express-async-handler";
 import userModel from "../model/user.js"
+import bcrypt from "bcryptjs"
+import gravatar from "gravatar"
 
 const router = express.Router()
 
 //CRUD
 //회원가입
 router.post("/signup", asyncHandler(async (req, res) => {
+
+    // email 유무 체크 -> 패스워드 암호화 -> 프로필 이미지 자동 생성
+
+    const user = await userModel.findOne({email: req.body.email})
+
+    if (user) {
+        return res.json({
+            msg: "email existed"
+        })
+
+    } else {
+
+        // 패스워드 암호화
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+
+        // 프로필 이미지 제너레이터
+        const profileImg = await gravatar.url(req.body.email, {
+            s: `200`,
+            r: `pg`,
+            d: `mm`,
+            protocol: `https`
+        })
+
+
+        const newUser = new userModel({
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword,
+            profileImg: profileImg
+        })
+
+        await newUser.save()
+
+        res.json({
+            msg: "created user",
+            userInfo: newUser
+        })
+
+    }
+
 
 }))
 
